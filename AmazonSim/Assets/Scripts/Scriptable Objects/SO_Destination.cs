184656c7a414f5da06c2922308b24db25f4c0a66
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2018-10-04 13:32:15 -0600

    added drone fuel ui

--- AmazonSim/Assets/Scripts/Scriptable Objects/SO_Destination.cs
+++ AmazonSim/Assets/Scripts/Scriptable Objects/SO_Destination.cs
@@ -7,7 +7,16 @@ using UnityEngine;
 public class SO_Destination : ScriptableObject {
 
 	public string destName;
-	public SO_Package destPkg;
-	public float distance;
+	public string pkgName;
+
+	public float pkgWgtMin = 0.1f;
+	public float pkgWgtMax = 5;
+	public float pkgWeight;
+
+
+	public void RandomizePackageWeight()
+	{
+		pkgWeight = Random.Range(pkgWgtMin, pkgWgtMax);
+	}
 
 }
